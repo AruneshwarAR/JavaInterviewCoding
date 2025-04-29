82f64502a44d07670bb62f6e4e02c258769d9168
Aruneshwar AR <hiaruneshwar@gmail.com>
2025-04-29 22:46:10 +0530

    added streamPractice

--- src/com/arun/practice/CountCharacters.java
+++ src/com/arun/practice/CountCharacters.java
@@ -6,6 +6,8 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 public class CountCharacters {
+	
+	
 	public static void main(String[] args) {
 		String str = "programming";
 		Map<Character, Integer> countChar = new HashMap<Character, Integer>();
@@ -16,7 +18,7 @@ public class CountCharacters {
 		System.out.println(countChar.size());
 		for(Map.Entry<Character , Integer > e :countChar.entrySet()) {
 			if (e.getValue() ==1) {
-				System.out.println(e);
+				 System.out.println("output: "+ e.getKey());
 			}
 			
 		}
