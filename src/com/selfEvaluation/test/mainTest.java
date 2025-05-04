67d71e4132435d4ebbc343ef0bd13984fcb1aab6
ARUNESHWAR AR <aruneshwarar@ARs-MBP.local>
2025-05-04 22:17:47 +0530

    inheritance test

---
+++ src/com/selfEvaluation/test/mainTest.java
@@ -0,0 +1,36 @@
+package com.selfEvaluation.test;
+
+public class mainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bike appuBike = new Bike("passion pro");
+		System.out.println(appuBike.displayInfo());
+		
+		Car appuCar = new Car("TATA","Curvv",2000000);
+		Car sowmiCar = new Car("TATA","Curvv.ev",1800000);
+		System.out.println(appuCar.getBrand());
+		System.out.println(appuCar.getModel());
+		System.out.println(appuCar.getPrice());
+		System.out.println(sowmiCar.getBrand());
+		System.out.println(sowmiCar.getModel());
+		System.out.println(sowmiCar.getPrice());
+		
+		appuCar.setBrand("Porche");
+		appuCar.setModel("Caymen");
+		appuCar.setPrice(150000000);
+		
+		sowmiCar.setBrand("Lamborghini");
+		sowmiCar.setModel("urus");
+		sowmiCar.setPrice(420000000);
+		
+		System.out.println(appuCar.getBrand());
+		System.out.println(appuCar.getModel());
+		System.out.println(appuCar.getPrice());
+		System.out.println(sowmiCar.getBrand());
+		System.out.println(sowmiCar.getModel());
+		System.out.println(sowmiCar.getPrice());
+		
+	}
+
+}
