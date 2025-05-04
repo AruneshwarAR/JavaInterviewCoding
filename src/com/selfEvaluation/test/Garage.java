d0a968a31ba99d459ec0d90e6bce77fca50ec4ab
ARUNESHWAR AR <aruneshwarar@ARs-MBP.local>
2025-05-04 23:53:15 +0530

    collectionFrameWork

---
+++ src/com/selfEvaluation/test/Garage.java
@@ -0,0 +1,11 @@
+package com.selfEvaluation.test;
+import java.util.*;
+public class Garage {
+	
+	Car appuCar = new Car("TATA","Curvv",2000000);
+	Car sowmiCar = new Car("TATA","Curvv.ev",1800000);
+	
+	List<Car> garage = new ArrayList<>();
+	
+	
+}
